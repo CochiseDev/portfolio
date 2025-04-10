10d839314c97ea44cdc056cfbf0b5dd3e3be7b6b
Cochise Cabau Torres <cochise.dev@gmail.com>
2025-04-10 12:05:38 +0200

    Updated last modification date

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Last updated: November 2024")}
+          {emoji("Last updated: April 2025")}
         </p>
       </div>
     </Fade>
